4947acd39c24eb770526c85df5868d57f08a0f2d
JinsoRaj <jinso.trv18it032@gecbh.ac.in>
2021-07-28 00:08:03 +0530

    ui logic

--- tennis/game.js
+++ tennis/game.js
@@ -1,6 +1,14 @@
 var canvas, canvasContext;
+
 var ballX = 50;
-var ballXSpeed = 5;
+var ballXSpeed = 10;
+var ballY = 50;
+var ballYSpeed = 5;
+
+const BAT_HEIGHT = 100;
+const BAT_THICK = 10;
+var bat1Y = 250;
+var bat2Y = 250;
 
 window.onload = function (){
     canvas = document.getElementById("gameCanvas");
@@ -11,30 +19,81 @@ window.onload = function (){
         moveElements();
         drawCanvas();
     }, 1000/FPS);
+
+    canvas.addEventListener("mousemove",
+    function(event) {
+        var mousePosition = findMousePosition(event);
+        bat1Y = mousePosition.y -(BAT_HEIGHT/2);
+    }
+);
+}
+
+function findMousePosition(event){
+    var rect = canvas.getBoundingClientRect();
+    var root = document.documentElement;
+    var mouseX = event.clientX - rect.left - root.scrollLeft;
+    var mouseY = event.clientY - rect.top - root.scrollTop;
+    return {
+        x:mouseX,
+        y:mouseY
+    };
 }
 
 function moveElements() {
     ballX = ballX + ballXSpeed;
+    ballY = ballY + ballYSpeed;
     //ballXSpeed = ballXSpeed + 1;
 
     //to left
     if (ballX >= canvas.width) {
-        ballXSpeed = -ballXSpeed;
+        if (ballY > bat2Y && ballY < bat2Y+ BAT_HEIGHT) {
+            ballXSpeed = -ballXSpeed;
+        } else {
+            reset();
+        }
     }
 
     //to right
     if (ballX <= 0) {
-        ballXSpeed = -ballXSpeed;
+        if (ballY > bat1Y && ballY < bat1Y+ BAT_HEIGHT) {
+            ballXSpeed = -ballXSpeed;
+        } else {
+            reset();
+        }
+    }
+
+    if (ballY >= canvas.height) {
+        ballYSpeed = -ballYSpeed;
+    }
+
+    //to right
+    if (ballY <= 0) {
+        ballYSpeed = -ballYSpeed;
     }
 }
 
 function drawCanvas() {
-    makeElement(0,0,canvas.width,canvas.height,"black"); //full canvas
-    makeElement(0,210,10,100,"white"); //bat
-    makeElement(ballX,100,10,10,"red"); //ball
+    createRectangle(0,0,canvas.width,canvas.height,"black"); //full canvas
+    createRectangle(0,bat1Y,BAT_THICK,BAT_HEIGHT,"white"); //bat1
+    createRectangle(canvas.width-BAT_THICK,bat2Y,BAT_THICK,BAT_HEIGHT,"white"); //bat2
+    createBall(ballX, ballY, 10, "white"); //ball
+    
 }
 
-function makeElement(leftX,topY,width,height,color){
+function createBall(centerX, centerY, radius, color) {
+    canvasContext.fillStyle = color;
+    canvasContext.beginPath();
+    canvasContext.arc(centerX, centerY, radius, 0, Math.PI*2, true);
+    canvasContext.fill();
+}
+
+function createRectangle(leftX,topY,width,height,color){
     canvasContext.fillStyle = color;
     canvasContext.fillRect(leftX,topY,width,height);
+}
+
+function reset() {
+    ballXSpeed = -ballXSpeed;
+    ballX = canvas.width/2;
+    ballY = canvas.height/2;
 }
